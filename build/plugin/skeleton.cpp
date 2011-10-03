72de74ccf7d2f97e4814ba4f20f4be0442f51188
huntercool <devnull@localhost>
2011-10-04 08:03:08 +1100

    added a skeleton plugin for underc

---
+++ build/plugin/skeleton.cpp
@@ -0,0 +1,45 @@
+#include <windows.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <map>
+#include <foreach.h>
+#include <axf/pluginapi.h>
+
+using namespace std; // for convinience
+
+// 0. Make sure everything is wrapped in a namespace because 
+//    underc shares names globally (which is a very bad idea!)
+namespace skeleton 
+{ 
+// 1. declare our plugin version
+const unsigned int VERSION = 1;
+
+// 2. declare our entry point
+WsBool OnInit(const PluginInterface *pi); 
+
+// 3. export the plugin description
+AXF_PLUGIN_DESCRIPTION(VERSION, OnInit, "Skeleton Plugin", "Your Name Here", "A dumb Plugin that does nothing")
+
+// 4. implementation, etc
+PluginInterfaceEx pi;
+
+WsBool OnInit(const PluginInterface *p)
+{
+    pi = p; // initialize PluginInterfaceEx (the C++ version of PluginInterface)
+
+    pi.Log("It's ready!");
+    pi.Log2(pi.Warn(), "Go write something useful you slacker!");
+     
+    // all plugins has to return WSTRUE
+    // if an error occurs during initialization, return WSFALSE instead
+    return WSTRUE;
+    
+    // All hooks, allocated interfaces and subscribed events will be released when you unload this plugin
+}
+
+} // namespace skeleton
+
+
+
