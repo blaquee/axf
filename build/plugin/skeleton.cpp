50825661033438eb1f8b9f8a18af1cf4586cc130
huntercool <devnull@localhost>
2011-10-04 08:15:14 +1100

    fixed up skeleton plugin

--- build/plugin/skeleton.cpp
+++ build/plugin/skeleton.cpp
@@ -13,16 +13,21 @@ using namespace std; // for convinience
 //    underc shares names globally (which is a very bad idea!)
 namespace skeleton 
 { 
-// 1. declare our plugin version
+// 1. declare our plugin version, has to be greater than 0
 const unsigned int VERSION = 1;
 
 // 2. declare our entry point
 WsBool OnInit(const PluginInterface *pi); 
+} // namespace skeleton
 
-// 3. export the plugin description
-AXF_PLUGIN_DESCRIPTION(VERSION, OnInit, "Skeleton Plugin", "Your Name Here", "A dumb Plugin that does nothing")
+
+// 3. export the plugin description, please use this macro outside the namespace
+AXF_PLUGIN_DESCRIPTION(skeleton::VERSION, skeleton::OnInit, "Skeleton Plugin", "Your Name Here", "A dumb Plugin that does nothing")
+
 
 // 4. implementation, etc
+namespace skeleton
+{
 PluginInterfaceEx pi;
 
 WsBool OnInit(const PluginInterface *p)
@@ -40,6 +45,4 @@ WsBool OnInit(const PluginInterface *p)
 }
 
 } // namespace skeleton
-
-
 
